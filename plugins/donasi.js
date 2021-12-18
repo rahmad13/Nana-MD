ad0fe3474688684ab79400b299cc4dbc993653a1
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-18 11:40:08 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,14 +5,14 @@ let handler = async (m) => {
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├🏧 081298108109 (OVO/Dana)
+├🏧 083108164709 (Dana)
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 
 Arigatou!
 
 Contact person Owner:
-wa.me/6281390199407 (Owner)`)
+wa.me/6283108164709 (Owner)`)
 }
 
 handler.help = ['donasi']
