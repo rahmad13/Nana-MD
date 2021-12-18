0f19ed042e535a4ab6173e73f60077e07476e450
Rhmd <93415263+rahmad13@users.noreply.github.com>
2021-12-18 11:41:23 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,12 +2,12 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Adul Alhy;;;'
-                    + 'FN:Adul Alhy\n' // full name
-                    + 'ORG:Adul Alhy;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 813-9019-9407\n' // WhatsApp ID + phone number
+                    + 'N:;Rhmd;;;'
+                    + 'FN:Rhmd\n' // full name
+                    + 'ORG:Rhmd;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6283108164709:+62 831-0816-4709\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Adul Alhy', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Rhmd', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
